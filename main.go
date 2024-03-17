bf93e12872ad4a0d78d0628806764e4c6d64b56f
yrgk <yarik090908@gmail.com>
2024-03-17 22:40:51 +0300

    created function to parse one game

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
-func main(){
+type Game struct {
+	name string
+	price string
+	coverUrl string
+	description string
+	publisher string
+}
 
+func main(){
+	for _, game := range getAllNames() {
+		fmt.Println(getOneItem(game))
+	}
+	// fmt.Println(getOneItem("https://store.playstation.com/en-us/concept/10000886"))
+	// fmt.Println(getOneItem("https://store.playstation.com/en-us/concept/232654"))
+	// fmt.Println(getOneItem("https://store.playstation.com/en-us/concept/231096"))
+}
+
+
+func getOneItem(link string) Game {
+	res, err := http.Get(link)
+
+	if err != nil {
+		log.Fatal("Failed to parse document", err)
+	}
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	if err != nil {
+		log.Fatal("Failed to parse document", err)
+	}
+	var game Game
+
+	game.name = doc.Find("h1[data-qa='mfe-game-title#name']").First().Text()
+	game.description = doc.Find("p[data-qa='mfe-game-overview#description']").First().Text()
+
+	return game
 }
 
 
